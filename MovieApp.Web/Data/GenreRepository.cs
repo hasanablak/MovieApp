2fafdae6f6d8adb9c8667a2a57fd3627be7a5cbc
Hasan Ablak <0hasanablak@gmail.com>
2024-08-18 21:59:03 +0300

    Repository
    Genre ve Movie repository oluşturuldu Modellere Id alanları eklenildi

---
+++ MovieApp.Web/Data/GenreRepository.cs
@@ -0,0 +1,50 @@
+﻿using MovieApp.Web.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MovieApp.Web.Data
+{
+    public class GenreRepository
+    {
+        private static readonly List<Genre> _genres = null;
+
+        static GenreRepository()
+        {
+            _genres = new List<Genre>()
+            {
+                new Genre
+                {
+                    GenreId = 1,
+                    Name = "Macera"
+                },
+                new Genre
+                {
+                    GenreId = 2,
+                    Name = "Komedi"
+                },
+                new Genre
+                {
+                    GenreId = 3,
+                    Name = "Bilim Kurgu"
+                }
+            };
+        }
+
+        public static List<Genre> Genres
+        {
+            get { return _genres; }
+        }
+
+        public static Genre GetById(int id)
+        {
+
+            return _genres.FirstOrDefault(genre => genre.GenreId == id);
+            
+        }
+
+        public static void Add(Genre genre)
+        {
+            _genres.Add(genre);
+        }
+    }
+}
---
+++ MovieApp.Web/Data/MovieRepository.cs
@@ -0,0 +1,69 @@
+﻿using MovieApp.Web.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MovieApp.Web.Data
+{
+    public class MovieRepository
+    {
+        private static readonly List<Movie> _movies = null;
+
+        static MovieRepository()
+        {
+            _movies = new List<Movie>()
+            {
+                new Movie{
+                    MovieId = 1,
+                    Title = "Film1",
+                    Director = "Director1",
+                    Description = "Description1",
+                    Players = new string[]{"player1.1", "player1.2"},
+                    ImageUrl = "attack.jpg"
+                },
+
+                new Movie{
+                    MovieId = 2,
+                    Title = "Film2",
+                    Director = "Director2",
+                    Description = "Description2",
+                    Players = new string[]{"player2.1", "player2.2"},
+                    ImageUrl = "batman.jpg"
+                },
+
+                new Movie{
+                    MovieId = 3,
+                    Title = "Film3",
+                    Director = "Director3",
+                    Description = "Description3",
+                    Players = new string[]{"player3.1", "player3.2"},
+                    ImageUrl = "attack.jpg"
+                },
+
+                new Movie{
+                    MovieId = 4,
+                    Title = "Film3",
+                    Director = "Director3",
+                    Description = "Description3",
+                    Players = new string[]{"player4.1", "player4.2"},
+                    ImageUrl = "batman.jpg"
+                },
+
+
+            };
+        }
+
+        public static List<Movie> Movies
+        {
+            get { return _movies; }
+        }
+
+        public static void Add(Movie movie){
+            _movies.Add(movie);
+        }
+
+        public static Movie GetById(int id)
+        {
+            return _movies.FirstOrDefault(movie => movie.MovieId == id);
+        }
+    }
+}
