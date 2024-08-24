8490a3a45dfb052b7e2b1ac8aca85d09f1ba959c
Hasan Ablak <0hasanablak@gmail.com>
2024-08-24 21:40:40 +0300

    Connection string eklendi

---
+++ MovieApp.Web/Data/MovieContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApp.Web.Entity;
+
+namespace MovieApp.Web.Data
+{
+    public class MovieContext: DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options):base(options)
+        {
+          
+        }
+        public DbSet<Movie> Movies { get; set; }
+
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<User> Users { get; set; }
+
+         /*
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=movies.db"); 
+        }
+         */
+    }
+}
