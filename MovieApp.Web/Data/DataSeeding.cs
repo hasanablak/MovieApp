54c8fc253c1f8ca19a2305f73c66cb7cfcc0260e
Hasan Ablak <0hasanablak@gmail.com>
2024-08-26 04:59:04 +0300

    Static bir class ve method içerisinde context nesnesine ulaşmak için static method içerisinde bir scope oluşturuluyor

---
+++ MovieApp.Web/Data/DataSeeding.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MovieApp.Web.Data
+{
+    public static class DataSeeding
+    {
+
+        public static void Seed(IApplicationBuilder app)
+        {
+            var scope = app.ApplicationServices.CreateScope();
+
+            var context = scope.ServiceProvider.GetService<MovieContext>();
+
+        }
+    }
+}
