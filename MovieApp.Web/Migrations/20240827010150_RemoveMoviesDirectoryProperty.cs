c8c7ff9ec17968649b884996e25c00f310bee10d
Hasan Ablak <0hasanablak@gmail.com>
2024-08-27 04:10:05 +0300

    Migration oluşturma RemoveMoviesDirectoryProperty
    dotnet ef migrations add RemoveMoviesDirectoryProperty
    dotnet ef database update

---
+++ MovieApp.Web/Migrations/20240827010150_RemoveMoviesDirectoryProperty.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieApp.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveMoviesDirectoryProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Director",
+                table: "Movies");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Director",
+                table: "Movies",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
