9afda17913fca868db3287f6d56df8e49f13ae5f
Hasan Ablak <0hasanablak@gmail.com>
2024-09-09 22:52:36 +0300

    database force yöntemi ile northwind database'i oluşturup, entityler oluşturuldu.
    dotnet ef dbcontext scaffold "Server=127.0.0.1;Port=3308;Database=Northwind;Uid=root;Pwd=" "Pomelo.EntityFrameworkCore.MySql" --output-dir "Data" --context NorthwindContext

---
+++ LINQSamples/Data/CustomerDemographic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LINQSamples.Data;
+
+public partial class CustomerDemographic
+{
+    public int CustomerTypeId { get; set; }
+
+    public string? CustomerDesc { get; set; }
+
+    public virtual ICollection<Customer> Custs { get; set; } = new List<Customer>();
+}
