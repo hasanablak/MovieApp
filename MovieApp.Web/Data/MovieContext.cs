de5c4dc5b962c6f358d110ff92c0ef2b5ed13a65
Hasan Ablak <0hasanablak@gmail.com>
2024-09-09 21:48:06 +0300

    Fluent api
    Fluent api ile context kurulduktan sonra migrations oluşturulurken,
    
    OnModelCreating methodunu ezerek bazı kurallar koyuyoruz, bu kurallar doğrultusunda modellerimiz ve karşılık gelen tablolarımız oluşumunu gerçekleştiriliyor.

--- MovieApp.Web/Data/MovieContext.cs
+++ MovieApp.Web/Data/MovieContext.cs
@@ -3,26 +3,45 @@ using MovieApp.Web.Entity;
 
 namespace MovieApp.Web.Data
 {
-    public class MovieContext: DbContext
-    {
-        public MovieContext(DbContextOptions<MovieContext> options):base(options)
-        {
-          
-        }
-        public DbSet<Movie> Movies { get; set; }
+	public class MovieContext : DbContext
+	{
+		public MovieContext(DbContextOptions<MovieContext> options) : base(options)
+		{
 
-        public DbSet <Person> People { get; set; }
+		}
+		public DbSet<Movie> Movies { get; set; }
 
-        public DbSet<Crew> Crews { get; set; }
-        public DbSet<Cast> Casts { get; set; }
+		public DbSet<Person> People { get; set; }
 
-
-
-        public DbSet<Genre> Genres { get; set; }
+		public DbSet<Crew> Crews { get; set; }
+		public DbSet<Cast> Casts { get; set; }
 
 
-        public DbSet<User> Users { get; set; }
 
-      
-    }
+		public DbSet<Genre> Genres { get; set; }
+
+
+		public DbSet<User> Users { get; set; }
+
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Movie>()
+			.Property(prop => prop.Title)
+			.IsRequired();
+
+			modelBuilder.Entity<Movie>()
+			.Property(prop => prop.Title)
+			.HasMaxLength(500);
+
+
+			modelBuilder.Entity<Genre>()
+			.Property(prop => prop.Name)
+			.IsRequired();
+
+			modelBuilder.Entity<Genre>()
+			.Property(prop => prop.Name)
+			.HasMaxLength(50);
+		}
+	}
 }
